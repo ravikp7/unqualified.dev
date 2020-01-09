607e69f151bba2bb4edea283f9ff24d5848b4da0
Ravi Kumar Prasad <7ravikp@gmail.com>
2020-01-09 20:07:08 +0530

    feat: add all blogs page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,16 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-import { Styled, css } from "theme-ui"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostCard from "../components/post-card"
-import ContactForm from "../components/contact-form"
 
-const IndexPage = ({ data }) => {
+const BlogPage = ({ data }) => {
   const { edges } = data.allMdx
   return (
     <Layout>
       <SEO title="Home" />
-      <p>Latest Blog Posts</p>
+      <p>All Blog Posts</p>
       {edges.map(({ node }) => {
         const { id, frontmatter, excerpt, fields, timeToRead } = node
         const { title, date, categories, tags } = frontmatter
@@ -25,32 +23,15 @@ const IndexPage = ({ data }) => {
           />
         )
       })}
-      <div
-        style={{
-          width: `100%`,
-          textAlign: `right`,
-        }}
-      >
-        <Styled.a
-          as={Link}
-          to="/blog"
-          style={{
-            marginRight: 0,
-          }}
-        >
-          All Blog Posts →
-        </Styled.a>
-      </div>
-      <ContactForm />
     </Layout>
   )
 }
 
-export default IndexPage
+export default BlogPage
 
 export const query = graphql`
   {
-    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       edges {
         node {
           id
