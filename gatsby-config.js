882d1486a31ed487302c3a9376691546cbdb22e6
Ravi Kumar Prasad <7ravikp@gmail.com>
2020-02-16 22:47:36 +0530

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,5 +84,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-158585372-1",
+      },
+    },
   ],
 }
